de9abd6eb2ae07791cdedc6c44d7c51dd16b0efe
Jerakin <the_rensvind@hotmail.com>
2025-01-07 17:05:47 +0100

    colemak layout

--- keyboards/keychron/q6_max/iso_encoder/keymaps/jerakin/keymap.c
+++ keyboards/keychron/q6_max/iso_encoder/keymaps/jerakin/keymap.c
@@ -24,11 +24,13 @@ enum layers {
     WIN_FN,
     _JK_BASE,
     _JK_NAV,
+    _JK_COLEMAK_DH,
 };
 
 
 #define _JK_BASE 4
 #define _JK_NAV 5
+#define _JK_COLEMAK_DH 6
 
 // Define the mod-tap keys for the home row
 #define HOME_A LGUI_T(KC_A)  // A acts as GUI (Cmd/Win) when held
@@ -92,6 +94,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,  _______,    _______,                                  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,              _______,            _______,  _______,  _______,  
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,            _______,  _______),
+
+    [_JK_COLEMAK_DH] = LAYOUT_iso_110(
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,    KC_PSCR,  KC_CTANA, RGB_MOD,  DF(WIN_BASE), _______,  _______,  _______,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_EQL,   KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,       KC_PSLS,  KC_PAST,  KC_PMNS,
+        KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_LBRC,  KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_SLASH,               KC_DEL,   KC_END,   KC_PGDN,  KC_P7,        KC_P8,    KC_P9,
+        KC_CAPS,  KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_RBRC,  KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,    KC_ENT,                                   KC_P4,        KC_P5,    KC_P6,    KC_PPLS,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,     KC_SLSH,  KC_NUHS,  KC_K,     KC_H,     KC_COMM,  KC_DOT,               KC_RSFT,              KC_UP,              KC_P1,        KC_P2,    KC_P3,
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,        KC_PDOT,  KC_PENT),
 };
 
 // clang-format on
@@ -103,6 +113,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [WIN_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
     [_JK_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [_JK_NAV]  = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [_JK_COLEMAK_DH]  = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
 };
 #endif // ENCODER_MAP_ENABLE
 
