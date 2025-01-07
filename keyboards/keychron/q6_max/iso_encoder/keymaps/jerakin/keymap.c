6bda1062e8b24dfa621af98bc6e492c058e2255c
Jerakin <the_rensvind@hotmail.com>
2025-01-07 17:04:25 +0100

    personal layout

--- keyboards/keychron/q6_max/iso_encoder/keymaps/jerakin/keymap.c
+++ keyboards/keychron/q6_max/iso_encoder/keymaps/jerakin/keymap.c
@@ -22,6 +22,7 @@ enum layers {
     MAC_FN,
     WIN_BASE,
     WIN_FN,
+    _JK_BASE,
 };
 
 
@@ -73,12 +74,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,            _______,  _______),
 
      [_JK_BASE] = LAYOUT_iso_110(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,   KC_F12,     KC_MUTE,    KC_PSCR,  KC_CTANA, RGB_MOD,  DF(WIN_BASE), _______,  _______,  _______,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,       KC_PSLS,  KC_PAST,  KC_PMNS,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_LBRC,  KC_RBRC,                KC_DEL,   KC_END,   KC_PGDN,  KC_P7,        KC_P8,    KC_P9,
-        KC_CAPS,  HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN, KC_QUOT,  KC_NUHS,    KC_ENT,                                   KC_P4,        KC_P5,    KC_P6,    KC_PPLS,
-        KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,              KC_RSFT,              KC_UP,              KC_P1,        KC_P2,    KC_P3,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,   KC_RWIN,  MO(WIN_FN), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,        KC_PDOT,  KC_PENT)
+        KC_ESC,   KC_F1,     KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,   KC_F12,     KC_MUTE,    KC_PSCR,  KC_CTANA, RGB_MOD,  DF(_JK_COLEMAK_DH), _______,  _______,  _______,
+        KC_GRV,   KC_1,      KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,             KC_PSLS,  KC_PAST,  KC_PMNS,
+        KC_TAB,   KC_Q,      KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_LBRC,  KC_RBRC,                KC_DEL,   KC_END,   KC_PGDN,  KC_P7,              KC_P8,    KC_P9,
+        KC_CAPS,  HOME_A,    HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN, KC_QUOT,  KC_NUHS,    KC_ENT,                                   KC_P4,              KC_P5,    KC_P6,    KC_PPLS,
+        KC_LSFT,  KC_NUBS,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,              KC_RSFT,              KC_UP,              KC_P1,              KC_P2,    KC_P3,
+        KC_LCTL,  KC_LWIN,   KC_LALT,                                KC_SPC,                                 KC_RALT,   KC_RWIN,  MO(WIN_FN), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),
 };
 
 // clang-format on
