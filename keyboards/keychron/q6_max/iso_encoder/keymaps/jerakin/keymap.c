ae4aec07ca122937130e96cadbfeb8294b242f3d
Jerakin <the_rensvind@hotmail.com>
2025-01-07 17:05:22 +0100

    navigation layout toggled by holding down q

--- keyboards/keychron/q6_max/iso_encoder/keymaps/jerakin/keymap.c
+++ keyboards/keychron/q6_max/iso_encoder/keymaps/jerakin/keymap.c
@@ -23,10 +23,12 @@ enum layers {
     WIN_BASE,
     WIN_FN,
     _JK_BASE,
+    _JK_NAV,
 };
 
 
 #define _JK_BASE 4
+#define _JK_NAV 5
 
 // Define the mod-tap keys for the home row
 #define HOME_A LGUI_T(KC_A)  // A acts as GUI (Cmd/Win) when held
@@ -38,6 +40,8 @@ enum layers {
 #define HOME_K RSFT_T(KC_K)  // K acts as Shift when held
 #define HOME_L RALT_T(KC_L)  // L acts as Alt when held
 #define HOME_SCLN RGUI_T(KC_SCLN)  // ; acts as GUI (Cmd/Win) when held
+
+#define LAYER_NAV LT(_JK_NAV, KC_Q)  // ; acts as GUI (Cmd/Win) when held
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -76,10 +80,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      [_JK_BASE] = LAYOUT_iso_110(
         KC_ESC,   KC_F1,     KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,   KC_F12,     KC_MUTE,    KC_PSCR,  KC_CTANA, RGB_MOD,  DF(_JK_COLEMAK_DH), _______,  _______,  _______,
         KC_GRV,   KC_1,      KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,             KC_PSLS,  KC_PAST,  KC_PMNS,
-        KC_TAB,   KC_Q,      KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_LBRC,  KC_RBRC,                KC_DEL,   KC_END,   KC_PGDN,  KC_P7,              KC_P8,    KC_P9,
+        KC_TAB,   LAYER_NAV, KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_LBRC,  KC_RBRC,                KC_DEL,   KC_END,   KC_PGDN,  KC_P7,              KC_P8,    KC_P9,
         KC_CAPS,  HOME_A,    HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN, KC_QUOT,  KC_NUHS,    KC_ENT,                                   KC_P4,              KC_P5,    KC_P6,    KC_PPLS,
         KC_LSFT,  KC_NUBS,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,              KC_RSFT,              KC_UP,              KC_P1,              KC_P2,    KC_P3,
         KC_LCTL,  KC_LWIN,   KC_LALT,                                KC_SPC,                                 KC_RALT,   KC_RWIN,  MO(WIN_FN), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),
+        
+     [_JK_NAV] = LAYOUT_iso_110(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,  _______,    _______,                                  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,              _______,            _______,  _______,  _______,  
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,            _______,  _______),
 };
 
 // clang-format on
@@ -90,6 +102,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [WIN_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
     [_JK_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [_JK_NAV]  = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
 };
 #endif // ENCODER_MAP_ENABLE
 
